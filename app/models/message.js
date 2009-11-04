adc5a66f6c5f8654069dd81c4c1acba0af83f57f
oil <brandonvb@gmail.com>
2009-11-04 12:43:13 -0800

    mostly complete message highlighting

--- app/models/message.js
+++ app/models/message.js
@@ -5,12 +5,14 @@ function ircMessage(params)
 	this.num =			ircMessage.num;
 	this.type =			params.type;
 	this.me =			params.me;
-	this.rowClass =		'';
-	this.rowStyle =		'';
 	this.nick =			false;
 	this.nickDisplay =	'';
-	this.nickStyle =	'';
 	this.message =		'';
+	
+	this.rowClass =		'';
+	this.rowStyle =		'';
+	this.nickStyle =	'';
+	
 	
 	switch(this.type)
 	{
@@ -20,9 +22,9 @@ function ircMessage(params)
 			this.nickStyle =	'color: ' + this.nick.colorHex + ';',
 			this.message =		params.message;
 			this.me =			params.me;
-			if (this.message.include(this.me) && this.nick.name != this.me) 
+			if (this.message.toLowerCase().include(this.me.toLowerCase()) && this.nick.name.toLowerCase() != this.me.toLowerCase()) 
 			{
-				this.rowStyle = 'background-color: ' + prefs.get().highlight;
+				this.highlightMessage();
 			}
 			break;
 			
@@ -31,9 +33,9 @@ function ircMessage(params)
 			this.nick =			params.nick;
 			this.nickDisplay =	'-*-';
 			this.message =		this.nick.name + ' ' + params.message;
-			if (this.message.include(this.me) && this.nick.name != this.me) 
+			if (this.message.toLowerCase().include(this.me.toLowerCase()) && this.nick.name.toLowerCase() != this.me.toLowerCase()) 
 			{
-				this.rowStyle = 'background-color: ' + prefs.get().highlight;
+				this.highlightMessage();
 			}
 			break;
 			
@@ -95,15 +97,55 @@ function ircMessage(params)
 	
 }
 
+ircMessage.prototype.highlightMessage = function()
+{
+	var style = '';
+	
+	if (prefs.get().highlightStyle == 'color' || prefs.get().highlightStyle == 'boldcolor') 
+	{
+		if (prefs.get().highlightColorOf == 'foreground') style += 'color: ' + prefs.get().highlightColor + ';';
+		if (prefs.get().highlightColorOf == 'background') style += 'background-color: ' + prefs.get().highlightColor + ';';
+	}
+	if (prefs.get().highlightStyle == 'bold' || prefs.get().highlightStyle == 'boldcolor') 
+	{
+		style += 'font-weight: bold;';
+	}
+	
+	switch (prefs.get().highlightPart)
+	{
+		case 'all':
+			this.rowStyle += style;
+			// foreground color doesn't work through children, sigh, so we have to set them manually...
+			if ((prefs.get().highlightStyle == 'color' ||
+				prefs.get().highlightStyle == 'boldcolor') &&
+				prefs.get().highlightColorOf == 'foreground')
+			{
+				this.nickStyle += style;
+				this.messageStyle += style;
+			}
+			break;
+		case 'nick':
+			this.nickStyle += style;
+			break;
+		case 'message':
+			this.messageStyle += style;
+			break;
+		case 'word':
+			this.message = this.message.replace(new RegExp('(' + this.me + ')', 'gi'), '<span style="' + style + '">$1</span>');
+			break;
+	}
+}
+
 ircMessage.prototype.getListObject = function()
 {
 	var obj =
 	{
-		rowClass:	this.rowClass,
-		rowStyle:	this.rowStyle,
-		nick:		this.nickDisplay,
-		nickStyle:	this.nickStyle,
-		message:		this.message
+		nick:			this.nickDisplay,
+		message:		this.message,
+		rowClass:		this.rowClass,
+		rowStyle:		this.rowStyle,
+		nickStyle:		this.nickStyle,
+		messageStyle:	this.messageStyle
 	};
 	
 	return obj;
