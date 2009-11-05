174e3516b13af3ec2727dc69167a5684a5b7047e
oil <brandonvb@gmail.com>
2009-11-05 01:15:34 -0800

    initial channel-users list tap dialog, nothing on it actually works yet

--- app/assistants/channel-users-assistant.js
+++ app/assistants/channel-users-assistant.js
@@ -30,8 +30,10 @@ ChannelUsersAssistant.prototype.setup = function()
 	{
 		this.controller.setupWidget(Mojo.Menu.appMenu, { omitDefaultItems: true }, this.menuModel);
 		
-		this.titleElement =				this.controller.get('title');
-		this.userListElement =			this.controller.get('userList');
+		this.titleElement =		this.controller.get('title');
+		this.userListElement =	this.controller.get('userList');
+		
+		this.listTapHandler =	this.listTap.bindAsEventListener(this);
 		
 		this.titleElement.innerHTML = this.channel.name;
 		
@@ -47,6 +49,9 @@ ChannelUsersAssistant.prototype.setup = function()
 			},
 			this.listModel
 		);
+		
+		Mojo.Event.listen(this.userListElement, Mojo.Event.listTap, this.listTapHandler);
+		
 	} 
 	catch (e) 
 	{
@@ -89,6 +94,16 @@ ChannelUsersAssistant.prototype.updateList = function(skipUpdate)
 	}
 }
 
+ChannelUsersAssistant.prototype.listTap = function(event)
+{
+	event.stop();
+	this.controller.showDialog(
+	{
+		template: 'channel-users/user-dialog',
+		assistant: new UserDialogAssistant(this, event.item)
+	});
+}
+
 ChannelUsersAssistant.prototype.handleCommand = function(event)
 {
 	if (event.type == Mojo.Event.command)
@@ -101,4 +116,129 @@ ChannelUsersAssistant.prototype.handleCommand = function(event)
 		}
 	}
 }
+
+ChannelUsersAssistant.prototype.cleanup = function(event)
+{
+	Mojo.Event.stopListening(this.userListElement, Mojo.Event.listTap, this.listTapHandler);
+}
+
+
+
+
+function UserDialogAssistant(sceneAssistant, item)
+{
+	this.sceneAssistant = sceneAssistant;
+	this.item = item;
+	
+	this.titleElement =			false;
+	this.queryButtonElement =	false;
+	this.opButtonElement =		false;
+	this.voiceButtonElement =	false;
+	this.kickButtonElement =	false;
+	this.banButtonElement =		false;
+	this.cancelButtonElement =	false;
+	
+	this.closeHandler =			false;
+}
+
+UserDialogAssistant.prototype.setup = function(widget)
+{
+	this.widget = widget;
+	
+	this.titleElement =			this.sceneAssistant.controller.get('dialogTitle');
+	this.queryButtonElement =	this.sceneAssistant.controller.get('queryButton');
+	this.opButtonElement =		this.sceneAssistant.controller.get('opButton');
+	this.voiceButtonElement =	this.sceneAssistant.controller.get('voiceButton');
+	this.kickButtonElement =	this.sceneAssistant.controller.get('kickButton');
+	this.banButtonElement =		this.sceneAssistant.controller.get('banButton');
+	this.cancelButtonElement =	this.sceneAssistant.controller.get('cancelButton');
+	
+	this.closeHandler =			this.close.bindAsEventListener(this);
+	
+	this.titleElement.update(this.item.name);
+	
+	
+	this.sceneAssistant.controller.setupWidget
+	(
+		'queryButton',
+		{},
+		{
+			buttonLabel: 'Query',
+			buttonClass: 'palm-button'
+		}
+	);
+	
+	this.sceneAssistant.controller.setupWidget
+	(
+		'opButton',
+		{},
+		{
+			buttonLabel: 'Op',
+			buttonClass: 'palm-button'
+		}
+	);
+	this.sceneAssistant.controller.setupWidget
+	(
+		'voiceButton',
+		{},
+		{
+			buttonLabel: 'Voice',
+			buttonClass: 'palm-button'
+		}
+	);
+	
+	this.sceneAssistant.controller.setupWidget
+	(
+		'kickButton',
+		{},
+		{
+			buttonLabel: 'Kick',
+			buttonClass: 'palm-button'
+		}
+	);
+	this.sceneAssistant.controller.setupWidget
+	(
+		'banButton',
+		{},
+		{
+			buttonLabel: 'Ban',
+			buttonClass: 'palm-button'
+		}
+	);
+	
+	this.sceneAssistant.controller.setupWidget
+	(
+		'cancelButton',
+		{},
+		{
+			buttonLabel: 'Cancel',
+			buttonClass: 'palm-button'
+		}
+	);
+	
+	
+	Mojo.Event.listen(this.queryButtonElement,	Mojo.Event.tap, this.closeHandler);
+	Mojo.Event.listen(this.opButtonElement,		Mojo.Event.tap, this.closeHandler);
+	Mojo.Event.listen(this.voiceButtonElement,	Mojo.Event.tap, this.closeHandler);
+	Mojo.Event.listen(this.kickButtonElement,	Mojo.Event.tap, this.closeHandler);
+	Mojo.Event.listen(this.banButtonElement,	Mojo.Event.tap, this.closeHandler);
+	Mojo.Event.listen(this.cancelButtonElement,	Mojo.Event.tap, this.closeHandler);
+}
+
+UserDialogAssistant.prototype.close = function(event)
+{
+	event.stop();
+	this.widget.mojo.close();
+}
+
+UserDialogAssistant.prototype.cleanup = function(event)
+{
+	Mojo.Event.stopListening(this.queryButtonElement,	Mojo.Event.tap, this.closeHandler);
+	Mojo.Event.stopListening(this.opButtonElement,		Mojo.Event.tap, this.closeHandler);
+	Mojo.Event.stopListening(this.voiceButtonElement,	Mojo.Event.tap, this.closeHandler);
+	Mojo.Event.stopListening(this.kickButtonElement,	Mojo.Event.tap, this.closeHandler);
+	Mojo.Event.stopListening(this.banButtonElement,	Mojo.Event.tap, this.closeHandler);
+	Mojo.Event.stopListening(this.cancelButtonElement,	Mojo.Event.tap, this.closeHandler);
+}
+
 
