361a1db2be551beddf9b768995b49dbfecc87fd1
Ryan Hope <rmh3093@gmail.com>
2009-11-12 01:14:08 -0500

    preview of preconfigured networks

--- app/models/preconfigured.js
+++ app/models/preconfigured.js
@@ -1,3 +1,61 @@
-var preconfigured = [];
+var preconfigured = [
 
-preconfigured.push({alias: 'Freenode',	address: 'irc.freenode.net'});
+	// Freenode
+	{network: 'Freenode',	region: '',			 		subregion: '', 					address: 'irc.freenode.net'},
+		
+	// Freenode (Europe)
+	{network: 'Freenode',	region: 'Europe', 			subregion: '', 					address: 'chat.eu.freenode.net'},
+	{network: 'Freenode',	region: 'Europe', 			subregion: 'Frankfurt, DE',		address: 'kornbluth.freenode.net'},
+	{network: 'Freenode',	region: 'Europe', 			subregion: 'Helsinki, FI',		address: 'orwell.freenode.net'},
+	{network: 'Freenode',	region: 'Europe', 			subregion: 'Milano, IT',		address: 'calvino.freenode.net'},
+	{network: 'Freenode',	region: 'Europe', 			subregion: 'Umeå, SE',			address: 'leguin.freenode.net'},
+	{network: 'Freenode',	region: 'Europe', 			subregion: 'Moscow, RU',		address: 'lem.freenode.net'},
+	{network: 'Freenode',	region: 'Europe', 			subregion: 'Manchester, UK',	address: 'wolfe.freenode.net'},
+	{network: 'Freenode',	region: 'Europe', 			subregion: 'Vilnius, LT',		address: 'sendak.freenode.net'},
+	{network: 'Freenode',	region: 'Europe', 			subregion: 'Evry, FR',			address: 'jordan.freenode.net'},
+	{network: 'Freenode',	region: 'Europe', 			subregion: 'Stockholm, SE',		address: 'lindbohm.freenode.net'},
+	{network: 'Freenode',	region: 'Europe', 			subregion: 'London, UK',		address: 'holmes.freenode.net'},
+	{network: 'Freenode',	region: 'Europe', 			subregion: 'Paris, FR',			address: 'barjavel.freenode.net'},
+	{network: 'Freenode',	region: 'Europe', 			subregion: 'Luxembourg, LU',	address: 'bartol.freenode.net'},
+	{network: 'Freenode',	region: 'Europe',			subregion: 'Rennes, FR',		address: 'pratchett.freenode.net'},
+	
+	// Freenode (United States)
+	{network: 'Freenode',	region: 'United States',	subregion: '',					address: 'chat.us.freenode.net'},
+	{network: 'Freenode',	region: 'United States',	subregion: 'Corvallis, OR',		address: 'niven.freenode.net'},
+	{network: 'Freenode',	region: 'United States',	subregion: 'Corvallis, OR',		address: 'zelazny.freenode.net'},
+	{network: 'Freenode',	region: 'United States',	subregion: 'Madison, WI',		address: 'brown.freenode.net'},
+	{network: 'Freenode',	region: 'United States',	subregion: 'Irvine, CA',		address: 'anthony.freenode.net'},
+	{network: 'Freenode',	region: 'United States',	subregion: 'Los Angeles, CA',	address: 'kubrick.freenode.net'},
+	{network: 'Freenode',	region: 'United States',	subregion: 'Newark, NJ',		address: 'verne.freenode.net'},
+	{network: 'Freenode',	region: 'United States',	subregion: 'Fremont, CA',		address: 'clarke.freenode.net'},
+	{network: 'Freenode',	region: 'United States',	subregion: 'Washington, DC',	address: 'card.freenode.net'},
+	{network: 'Freenode',	region: 'United States',	subregion: 'San Diego, CA',		address: 'simmons.freenode.net'},
+	{network: 'Freenode',	region: 'United States',	subregion: 'Pittsburgh, PA',	address: 'hubbard.freenode.net'},
+	
+	// Rizon
+	{network: 'Rizon',		region: '',					subregion: '',					address: 'irc.rizon.net'},
+	
+	// Rizon (Europe)
+	{network: 'Rizon',		region: 'Europe',			subregion: 'DE',				address: 'irc.zipd.de'},
+	{network: 'Rizon',		region: 'Europe',			subregion: 'FR',				address: 'irc.lolipower.org'},
+	{network: 'Rizon',		region: 'Europe',			subregion: 'NL',				address: 'irc.impact-media.me.uk'},
+	{network: 'Rizon',		region: 'Europe',			subregion: 'NO',				address: 'irc.rizon.no'},
+	{network: 'Rizon',		region: 'Europe',			subregion: 'UK',				address: 'irc.shakeababy.net'},
+	
+	// Rizon (North America)
+	{network: 'Rizon',		region: 'North America',	subregion: 'CA',				address: 'irc.thefear.ca'},
+	{network: 'Rizon',		region: 'North America',	subregion: 'US',				address: 'irc.ashenworlds.net'},
+	{network: 'Rizon',		region: 'North America',	subregion: 'US',				address: 'irc.cccp-project.net'},
+	{network: 'Rizon',		region: 'North America',	subregion: 'US',				address: 'irc.cyberdynesystems.net'},
+	{network: 'Rizon',		region: 'North America',	subregion: 'US',				address: 'irc.dronebl.org'},
+	{network: 'Rizon',		region: 'North America',	subregion: 'US',				address: 'irc.gamepad.ca'},
+	{network: 'Rizon',		region: 'North America',	subregion: 'US',				address: 'irc.nitemare.name'},
+	{network: 'Rizon',		region: 'North America',	subregion: 'US',				address: 'irc.nuu.cc'},
+	{network: 'Rizon',		region: 'North America',	subregion: 'US',				address: 'irc.pantsuland.net'},
+	{network: 'Rizon',		region: 'North America',	subregion: 'US',				address: 'irc.x1n.org'},
+	
+	// Rizon (Asia)
+	{network: 'Rizon',		region: 'Asia',				subregion: 'RU',				address: 'irc.fujibayashi.jp'},
+	
+];
+
