9e8d771a7ae522d8dfb40fc65d650dd02896bae9
Ryan Hope <rmh3093@gmail.com>
2010-05-28 12:57:23 -0400

    cleanup

--- Makefile
+++ Makefile
@@ -16,3 +16,5 @@ clean-plugin:
 
 install: build-package
 	pdk-install `cat appinfo.json | grep id | cut -f 2 -d ':' | cut -f 2 -d '"'`_`cat appinfo.json | grep version | cut -f 2 -d ':' | cut -f 2 -d '"'`_all.ipk
+
+clean: clean-plugin clean-package
--- src/main.c
+++ src/main.c
@@ -16,75 +16,7 @@
  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  =============================================================================*/
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <getopt.h>
-
 #include "wIRC.h"
-
-static struct option long_options[] = {
-		{ "help",		no_argument,		0, 'h' },
-		{ "version",	no_argument,		0, 'V' },
-		{ "debug",		required_argument,	0, 'D' },
-		{ "retries",	required_argument,	0, 'r' },
-		{ "sleep",		required_argument,	0, 's' },
-		{ 0, 0, 0, 0 }
-};
-
-void print_version() {
-	printf("wIRCd - WebOS IRC Daemon (%s)\n", VERSION);
-}
-
-void print_help(char *argv[]) {
-
-	printf("Usage: %s [OPTION]...\n\n"
-		"Miscellaneous:\n"
-		"  -h, --help\t\tprint help information and exit\n"
-		"  -s, --sleep\t\tset the pre \"irc_run()\" micro sleep value (default is 500000)\n"
-		"  -r, --retries\t\tset the number of retries before {\"returnVal\":0}\n"
-		"  -D, --debug\t\tset debug level\n"
-		"  -V, --version\t\tprint version information and exit\n", argv[0]);
-
-}
-
-int getopts(int argc, char *argv[]) {
-
-	int c, retVal = 0;
-
-	while (1) {
-		int option_index = 0;
-		c = getopt_long(argc, argv, "D:Vhr:s:", long_options, &option_index);
-		if (c == -1)
-			break;
-		switch (c) {
-		case 'D':
-			debug = atoi(optarg);
-			break;
-		case 'V':
-			print_version();
-			retVal = 1;
-			break;
-		case 'h':
-			print_help(argv);
-			retVal = 1;
-			break;
-        case 'r':
-        	max_retries = atoi(optarg);
-        	break;
-        case 's':
-        	pre_run_usleep = atoi(optarg);
-        	break;
-		case '?':
-			print_help(argv);
-			retVal = 1;
-			break;
-		default:
-			abort();
-		}
-	}
-	return retVal;
-
-}
 
 int main(int argc, char *argv[]) {
 
@@ -93,9 +25,6 @@ int main(int argc, char *argv[]) {
 	max_retries = DEFAULT_MAX_RETRIES;
 	pre_run_usleep = DEFAULT_PRE_RUN_USLEEP;
 	debug = DEFAULT_DEBUG_LEVEL;
-
-	if (getopts(argc, argv) == 1)
-		return 1;
 
 	int ret = plugin_initialize();
     if (ret == PDL_NOERROR) {
