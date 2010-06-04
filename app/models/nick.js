761bcbd002e068960f1efc14c3db7efd6bd06806
Ryan Hope <rmh3093@gmail.com>
2010-06-04 17:14:59 -0400

    more coloring

--- app/models/nick.js
+++ app/models/nick.js
@@ -99,9 +99,65 @@ ircNick.prototype.getListObject = function(channel)
 	return obj;
 }
 
+ircNick.prototype.hsv2rgb = function(h, s, v)
+{
+	var r, g, b;
+	var RGB = new Array();
+	if (s==0) {
+  		RGB['red']=RGB['green']=RGB['blue']=Math.round(v*255);
+	} else {
+  		var var_h = h * 6;
+  		if (var_h==6) var_h = 0;
+ 		var var_i = Math.floor( var_h );
+  		var var_1 = v*(1-s);
+  		var var_2 = v*(1-s*(var_h-var_i));
+  		var var_3 = v*(1-s*(1-(var_h-var_i)));
+  		if(var_i==0){ 
+    		var_r = v; 
+    		var_g = var_3; 
+    		var_b = var_1;
+  		} else if (var_i==1) { 
+    		var_r = var_2;
+    		var_g = v;
+    		var_b = var_1;
+  		} else if(var_i==2) {
+    		var_r = var_1;
+    		var_g = v;
+    		var_b = var_3
+  		} else if (var_i==3) {
+    		var_r = var_1;
+    		var_g = var_2;
+    		var_b = v;
+  		} else if (var_i==4) {
+    		var_r = var_3;
+    		var_g = var_1;
+    		var_b = v;
+  		} else { 
+    		var_r = v;
+    		var_g = var_1;
+    		var_b = var_2
+  		}  	  
+  		RGB['red']=Math.round(var_r * 255);
+  		RGB['green']=Math.round(var_g * 255);
+  		RGB['blue']=Math.round(var_b * 255);
+  	}
+	return RGB;  
+};
+
+ircNick.prototype.getRandomColor3 = function()
+{
+	var hue = 0;
+	do
+	{
+		hue = Math.floor(Math.random()*360);
+	} while (55<hue && hue<90);
+			
+    return this.hsv2rgb(hue, 100, Math.floor(Math.random()*20)+80);
+}
+
 ircNick.prototype.getRandomColor2 = function()
 {
-	var rgb = [];
+	var rgb	 = [];
 	rgb.push(Math.floor(Math.random()*256));
 	rgb.push(Math.floor(Math.random()*256));
 	if (rgb[0] <= 64 || rgb[1] <= 64)
