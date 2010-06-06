e7717d23f4135866446f82e9e965560711ff7c85
Ryan Hope <rmh3093@gmail.com>
2010-06-06 13:00:05 -0400

    started a pseudo-random color picker with light/dark theme support

--- app/models/nick.js
+++ app/models/nick.js
@@ -5,7 +5,7 @@ function ircNick(params)
 	this.num =			ircNick.num;
 	this.name =			params.name;
 	this.server =		false; // set when whois is called, its the only time its needed
-	this.colorHex =		this.getRandomColor2();
+	this.colorHex =		this.getRandomColor3(true);
 	this.channels =		[];
 	this.channelModes =	[];
 	this.me =			false;
@@ -99,72 +99,17 @@ ircNick.prototype.getListObject = function(channel)
 	return obj;
 }
 
-ircNick.prototype.hsv2rgb = function(h, s, v)
+ircNick.prototype.getRandomColor3 = function(lightTheme)
 {
-	var r, g, b;
-	var RGB = new Array();
-	if (s==0) {
-  		RGB['red']=RGB['green']=RGB['blue']=Math.round(v*255);
-	} else {
-  		var var_h = h * 6;
-  		if (var_h==6) var_h = 0;
- 		var var_i = Math.floor( var_h );
-  		var var_1 = v*(1-s);
-  		var var_2 = v*(1-s*(var_h-var_i));
-  		var var_3 = v*(1-s*(1-(var_h-var_i)));
-  		if(var_i==0){ 
-    		var_r = v; 
-    		var_g = var_3; 
-    		var_b = var_1;
-  		} else if (var_i==1) { 
-    		var_r = var_2;
-    		var_g = v;
-    		var_b = var_1;
-  		} else if(var_i==2) {
-    		var_r = var_1;
-    		var_g = v;
-    		var_b = var_3
-  		} else if (var_i==3) {
-    		var_r = var_1;
-    		var_g = var_2;
-    		var_b = v;
-  		} else if (var_i==4) {
-    		var_r = var_3;
-    		var_g = var_1;
-    		var_b = v;
-  		} else { 
-    		var_r = v;
-    		var_g = var_1;
-    		var_b = var_2
-  		}  	  
-  		RGB['red']=Math.round(var_r * 255);
-  		RGB['green']=Math.round(var_g * 255);
-  		RGB['blue']=Math.round(var_b * 255);
-  	}
-	return RGB;  
-};
-
-ircNick.prototype.getRandomColor3 = function()
-{
-	var hue = 0;
-	do
-	{
-		hue = Math.floor(Math.random()*360);
-	} while (55<hue && hue<90);
-			
-    return this.hsv2rgb(hue, 100, Math.floor(Math.random()*20)+80);
-}
-
-ircNick.prototype.getRandomColor2 = function()
-{
-	var rgb	 = [];
-	rgb.push(Math.floor(Math.random()*256));
-	rgb.push(Math.floor(Math.random()*256));
-	if (rgb[0] <= 64 || rgb[1] <= 64)
-		rgb.push(Math.floor(Math.random()*256));
+	var hue = Math.ceil(Math.random()*360);
+	var saturation = 100;
+	var value = 100;
+	if (lightTheme)
+		value = Math.ceil(Math.random()*20)+70;
 	else
-		rgb.push(Math.floor(Math.random()*64));	
-    return "rgb(" + rgb[0] + "," + rgb[1] + "," + rgb[2] + ")";
+		saturation = Math.ceil(Math.random()*20)+60;
+	var rgb = ColorConverter.toRGB(new HSV(hue,saturation,value));
+	return "rgb(" + rgb.r + "," + rgb.b + "," + rgb.g + ")";
 }
 
 ircNick.prototype.getRandomColor = function()
--- app/models/utility.js
+++ app/models/utility.js
@@ -1,100 +1,102 @@
 // formats a timestamp to a readable date
-formatDate = function(date)
-{
-	var dateObj = new Date(date * 1000);
-	var toReturn = '';
-	var pm = false;
-	
-	toReturn += (dateObj.getMonth() + 1) + '/' + dateObj.getDate() + '/' + String(dateObj.getFullYear()).substring(2) + ' ';
-	
-	if (dateObj.getHours() > 12) pm = true;
-	
-	if (!pm)
-	{
-		if (dateObj.getHours() < 1) toReturn += '12';
-		if (dateObj.getHours() > 0) toReturn += dateObj.getHours();
-		toReturn += ':';
-		if (dateObj.getMinutes() < 10) toReturn += '0'
-		toReturn += dateObj.getMinutes() + ' AM';
-	}
-	else
-	{
-		toReturn += (dateObj.getHours() - 12) + ':';
-		if (dateObj.getMinutes() < 10) toReturn += '0'
-		toReturn += dateObj.getMinutes() + ' PM';
-	}
-	
-	return toReturn;
+formatDate = function(date){
+    var dateObj = new Date(date * 1000);
+    var toReturn = '';
+    var pm = false;
+    
+    toReturn += (dateObj.getMonth() + 1) + '/' + dateObj.getDate() + '/' + String(dateObj.getFullYear()).substring(2) + ' ';
+    
+    if (dateObj.getHours() > 12) 
+        pm = true;
+    
+    if (!pm) {
+        if (dateObj.getHours() < 1) 
+            toReturn += '12';
+        if (dateObj.getHours() > 0) 
+            toReturn += dateObj.getHours();
+        toReturn += ':';
+        if (dateObj.getMinutes() < 10) 
+            toReturn += '0'
+        toReturn += dateObj.getMinutes() + ' AM';
+    }
+    else {
+        toReturn += (dateObj.getHours() - 12) + ':';
+        if (dateObj.getMinutes() < 10) 
+            toReturn += '0'
+        toReturn += dateObj.getMinutes() + ' PM';
+    }
+    
+    return toReturn;
 }
 
 // strips html tags and fixes "double space" problem in the string
-formatForHtml = function(string)
-{
-	string = string.escapeHTML();
-	string = string.replace(/[\s]{2}/g, " &nbsp;");
-	return string;
+formatForHtml = function(string){
+    string = string.escapeHTML();
+    string = string.replace(/[\s]{2}/g, " &nbsp;");
+    return string;
 }
 
 // parses urls to html links, not using Mojo.Format.runTextIndexer because it tries to parse email addresses and other crap stupidly
-formatLinks = function(string)
-{
-  	return string.replace
-	(
-		/((https?\:\/\/)|(www\.))(\S+)(\w{2,4})(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/ig,
-		function(url)
-		{
-		    var full_url = url;
-		    if (!full_url.match('^https?:\/\/')) {
-		        full_url = 'http://' + full_url;
-		    }
-		    return '<a href="' + full_url + '" target="_blank">' + url + '</a>';
-		}
-	);
+formatLinks = function(string){
+    return string.replace(/((https?\:\/\/)|(www\.))(\S+)(\w{2,4})(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/ig, function(url){
+        var full_url = url;
+        if (!full_url.match('^https?:\/\/')) {
+            full_url = 'http://' + full_url;
+        }
+        return '<a href="' + full_url + '" target="_blank">' + url + '</a>';
+    });
 }
 
 // trim function
-trim = function(str)
-{
-	return str.replace(/^\s*/, "").replace(/\s*$/, "");
+trim = function(str){
+    return str.replace(/^\s*/, "").replace(/\s*$/, "");
 }
 
 // formats seconds into a readable string
-formatSeconds = function(seconds, longFormat)
-{
-	var toReturn = '';
-	
-	var days = Math.floor(seconds / 86400);
-	seconds -= (days * 86400);
-	
-	var hours = Math.floor(seconds / 3600);
-	seconds -= (hours * 3600);
-
-	var mins = Math.floor(seconds / 60);
-	seconds -= (mins * 60);
-	
-	if (longFormat) 
-	{
-		if (days == 1)		toReturn += days + ' day ';
-		if (days > 1)		toReturn += days + ' days ';
-		if (hours == 1)		toReturn += hours + ' hour ';
-		if (hours > 1)		toReturn += hours + ' hours ';
-		if (mins == 1)		toReturn += mins + ' min ';
-		if (mins > 1)		toReturn += mins + ' mins ';
-		if (seconds == 1)	toReturn += seconds + ' sec ';
-		if (seconds > 1)	toReturn += seconds + ' secs ';
-	}
-	else
-	{
-		if (days > 0)		toReturn += days + 'd ';
-		if (hours > 0)		toReturn += hours + 'h ';
-		if (mins > 0)		toReturn += mins + 'm ';
-		if (seconds > 0)	toReturn += seconds + 's ';
-	}
-	
-	if (toReturn == '')
-	{
-		toReturn = 'never';
-	}
-	
-	return toReturn;
+formatSeconds = function(seconds, longFormat){
+    var toReturn = '';
+    
+    var days = Math.floor(seconds / 86400);
+    seconds -= (days * 86400);
+    
+    var hours = Math.floor(seconds / 3600);
+    seconds -= (hours * 3600);
+    
+    var mins = Math.floor(seconds / 60);
+    seconds -= (mins * 60);
+    
+    if (longFormat) {
+        if (days == 1) 
+            toReturn += days + ' day ';
+        if (days > 1) 
+            toReturn += days + ' days ';
+        if (hours == 1) 
+            toReturn += hours + ' hour ';
+        if (hours > 1) 
+            toReturn += hours + ' hours ';
+        if (mins == 1) 
+            toReturn += mins + ' min ';
+        if (mins > 1) 
+            toReturn += mins + ' mins ';
+        if (seconds == 1) 
+            toReturn += seconds + ' sec ';
+        if (seconds > 1) 
+            toReturn += seconds + ' secs ';
+    }
+    else {
+        if (days > 0) 
+            toReturn += days + 'd ';
+        if (hours > 0) 
+            toReturn += hours + 'h ';
+        if (mins > 0) 
+            toReturn += mins + 'm ';
+        if (seconds > 0) 
+            toReturn += seconds + 's ';
+    }
+    
+    if (toReturn == '') {
+        toReturn = 'never';
+    }
+    
+    return toReturn;
 }
