04baf6d40ad60876f68e2c76351331ad041c0d07
Ryan Hope <rmh3093@gmail.com>
2010-06-12 09:24:28 -0400

    add another tagline

--- app/assistants/server-list-assistant.js
+++ app/assistants/server-list-assistant.js
@@ -6,6 +6,7 @@ function ServerListAssistant()
 		{weight: 30, text: 'The webOS IRC Client'},
 		{weight: 50, text: 'Coming soon... SSL!'},
 		{weight: 30, text: 'Mobile IRC Done Right'},
+		{weight: 20, text: 'More features than XChat'},
 		{weight:  8, text: 'Now you can IRC from the crapper'},
 		{weight:  2, text: 'You can, but can\'t'},
 		{weight:  2, text: 'Random Taglines Are Awesome'}
