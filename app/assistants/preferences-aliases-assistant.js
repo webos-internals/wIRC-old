716801617c7e676318270dca74b952b9d8dfb58a
oil <brandonvb@gmail.com>
2010-06-27 20:08:51 -0700

    added initial aliases prefs scene

---
+++ app/assistants/preferences-aliases-assistant.js
@@ -0,0 +1,130 @@
+function PreferencesAliasesAssistant()
+{
+	// setup default preferences in the prefCookie.js model
+	this.cookie = new preferenceCookie();
+	this.prefs = this.cookie.get();
+	
+	this.pageList = [
+		{label: 'General',			command: 'general'},
+		{label: 'Messages',			command: 'messages'},
+		{label: 'Events',			command: 'events'},
+		{label: 'Keybindings',		command: 'keybindings'},
+		{label: 'Notifications',	command: 'notifications'},
+		{label: 'DCC',				command: 'dcc'},
+		{label: 'Aliases',			command: 'aliases'}
+	];
+	this.currentPage = 'aliases';
+	
+	// setup menu
+	this.menuModel =
+	{
+		visible: true,
+		items:
+		[
+			Mojo.Menu.editItem,
+			{
+				label: "Help",
+				command: 'do-help'
+			}
+		]
+	}
+}
+
+PreferencesAliasesAssistant.prototype.setup = function()
+{
+	try
+	{
+		// setup menu
+		this.controller.setupWidget(Mojo.Menu.appMenu, { omitDefaultItems: true }, this.menuModel);
+		
+		// set this scene's default transition
+		this.controller.setDefaultTransition(Mojo.Transition.zoomFade);
+		
+		// setup page selector
+		this.pageSelectorElement =	this.controller.get('pageSelector');
+		this.pageNameElement =		this.controller.get('pageName');
+		this.pageTapHandler =		this.pageTap.bindAsEventListener(this);
+		this.pageSwitcher =			this.pageSwitch.bindAsEventListener(this);
+		this.controller.listen(this.pageSelectorElement, Mojo.Event.tap, this.pageTapHandler);
+		
+		this.pageNameElement.update(this.currentPage);
+		
+		// setup handlers for preferences
+		this.toggleChangeHandler = this.toggleChanged.bindAsEventListener(this);
+		this.sliderChangeHandler = this.sliderChanged.bindAsEventListener(this);
+		this.listChangedHandler  = this.listChanged.bindAsEventListener(this);
+				
+	}
+	catch (e)
+	{
+		Mojo.Log.logException(e, 'preferences#setup');
+	}
+
+}
+
+PreferencesAliasesAssistant.prototype.toggleChanged = function(event)
+{
+	this.prefs[event.target.id] = event.value;
+	this.cookie.put(this.prefs);
+}
+PreferencesAliasesAssistant.prototype.sliderChanged = function(event)
+{
+	this.cookie.put(this.prefs);
+}
+PreferencesAliasesAssistant.prototype.listChanged = function(event)
+{
+	this.cookie.put(this.prefs);
+}
+
+PreferencesAliasesAssistant.prototype.pageSwitch = function(page)
+{
+	if (page === null || page == "" || page == undefined || page == this.currentPage) return;
+	this.controller.stageController.swapScene({name: 'preferences-'+page, transition: Mojo.Transition.crossFade});
+}
+
+PreferencesAliasesAssistant.prototype.pageTap = function(event)
+{
+	this.controller.popupSubmenu(
+	{
+		onChoose: this.pageSwitcher,
+		popupClass: 'group-popup',
+		toggleCmd: this.currentPage,
+		placeNear: event.target,
+		items: this.pageList
+	});
+}
+
+PreferencesAliasesAssistant.prototype.handleCommand = function(event)
+{
+	if (event.type == Mojo.Event.command)
+	{
+		switch (event.command)
+		{
+			case 'do-help':
+				this.controller.stageController.pushScene('help');
+				break;
+		}
+	}
+}
+
+PreferencesAliasesAssistant.prototype.activate = function(event)
+{
+	if (!this.hasBennActivated)
+	{
+		this.pageSwitcher(this.currentPage);
+	}
+	this.hasBennActivated = true;
+}
+
+PreferencesAliasesAssistant.prototype.deactivate = function(event)
+{
+	this.alertListSave();
+	
+	// reload global storage of preferences when we get rid of this stage
+	var tmp = prefs.get(true);
+}
+
+PreferencesAliasesAssistant.prototype.cleanup = function(event)
+{
+	this.controller.stopListening(this.pageSelectorElement, Mojo.Event.tap,			   this.pageTapHandler);
+}
