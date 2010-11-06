6f877a3e238a17c279ce219729d6f7422884826d
oil <brandonvb@gmail.com>
2010-11-05 18:18:39 -0700

    "its pronounced werk" tagline

--- app/assistants/server-list-assistant.js
+++ app/assistants/server-list-assistant.js
@@ -5,6 +5,7 @@ function ServerListAssistant()
 	[
 		{weight: 30, text: 'The webOS IRC Client'},
 		{weight: 30, text: 'Mobile IRC Done Right'},
+		{weight:  8, text: 'It\'s pronounced "werk"'},
 		{weight:  8, text: 'Now you can IRC from the crapper'},
 		{weight:  8, text: 'Now you can IRC from the bar'},
 		{weight:  2, text: 'You can, but can\'t'},
