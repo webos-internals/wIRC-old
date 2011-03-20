d3684c2af6e7d77b943047d6996c34313cf5db9f
oil <brandonvb@gmail.com>
2011-03-19 18:03:58 -0700

    fix q/a prev/next command history in server-status (issue 97)

--- app/assistants/server-status-assistant.js
+++ app/assistants/server-status-assistant.js
@@ -174,26 +174,24 @@ ServerStatusAssistant.prototype.activate = function(event)
 
 ServerStatusAssistant.prototype.keyHandler = function(event){
 	
-    var isActionKey = (event.keyCode === Mojo.Char.metaKey);
 	var isCmdUp = (event.keyCode === Mojo.Char.q);
 	var isCmdDown = (event.keyCode === Mojo.Char.a);
-    
-    if (event.type === 'keydown' && isActionKey) {
-        this.action = true;
-    }
-    else if (event.type === 'keyup' && isActionKey) {
-		this.action = false;
-	}
-    
-	if (this.action && event.type == 'keyup' && cmdHistory.length>0) {
-		if (isCmdUp && cmdHistoryIndex<cmdHistory.length) cmdHistoryIndex++;
-		else if (isCmdDown && cmdHistoryIndex > 0) cmdHistoryIndex--;
-		if (cmdHistoryIndex==0)
-			this.inputWidgetElement.mojo.setValue('');
-		else
-			this.inputWidgetElement.mojo.setValue(cmdHistory[cmdHistory.length-cmdHistoryIndex]);
-	}
 	
+	if (event.metaKey) {
+		if (event.type === 'keyup') {
+			if (isCmdDown || isCmdUp) {
+				if (isCmdUp && cmdHistoryIndex<cmdHistory.length) 
+					cmdHistoryIndex++;
+				else 
+					if (isCmdDown && cmdHistoryIndex > 0) 
+						cmdHistoryIndex--;
+				if (cmdHistoryIndex == 0) 
+					this.inputWidgetElement.mojo.setValue('');
+				else 
+					this.inputWidgetElement.mojo.setValue(cmdHistory[cmdHistory.length-cmdHistoryIndex]);
+			} 
+		}
+	}
 }
 
 ServerStatusAssistant.prototype.updateList = function(initial)
