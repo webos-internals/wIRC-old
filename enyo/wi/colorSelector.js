a068d6902ebab980ee1541350fea1ff64bd71297
oil <brandonvb@gmail.com>
2011-08-15 02:38:27 -0700

    more work on still unfinished color picker

--- enyo/wi/colorSelector.js
+++ enyo/wi/colorSelector.js
@@ -53,13 +53,14 @@ enyo.kind({
 	scrim: false,
 	
 	style: 'margin-top: -60px; margin-left: -12px;',
+	popupBorderWidth: 24, // so says the css for the popup
 	
 	events: {
 		onColorSelect: ''
 	},
 	
 	components: [
-		{name: 'dragger', style: 'position: absolute; top: 0px; left 0px; width: 30px; height: 30px; background: #fff;'},
+		{name: 'dragger', style: 'position: absolute; top: 0px; left 0px; width: 60px; height: 60px; background: #fff; border-radius: 30px;'},
 		{
 			name: 'canvas',
 			kind: enyo.Control,
@@ -81,8 +82,8 @@ enyo.kind({
 		this.img.src = 'enyo/images/colors.png';
 		this.img.onload = enyo.bind(this, 'drawImage');
 		
-		this.$.dragger.applyStyle('top', '-50px');
-		this.$.dragger.applyStyle('left', '-50px');
+		this.$.dragger.applyStyle('top', '-200px');
+		this.$.dragger.applyStyle('left', '-200px');
 	},
 	doClose: function() {
 	},
@@ -109,25 +110,32 @@ enyo.kind({
 	
 	dragGetStartPosition: function(inSender, inEvent) {
 		var left, top;
-		left = inEvent.pageX - parseInt(this.node.style.left, 10);
-		top = inEvent.pageY - parseInt(this.node.style.top, 10);
-		// edit for positioning:
-		left = left - 20;
-		top = top - 10;
-		return {left: left, top: top}
+		left = inEvent.pageX - (parseInt(this.node.style.left, 10) + parseInt(this.node.style.marginLeft, 10) + this.popupBorderWidth);
+		top = inEvent.pageY - (parseInt(this.node.style.top, 10) + parseInt(this.node.style.marginTop, 10) + this.popupBorderWidth);
+		// edit for positioning above the finger?
+		left = left - (parseInt(this.$.dragger.node.style.width, 10) / 2);
+		top = top - (parseInt(this.$.dragger.node.style.height, 10) / 2) - 50;
+		return {left: left, top: top};
+	},
+	dragGetCanvasPosition: function(inSender, inEvent) {
+		var x, y;
+		x = inEvent.pageX - (parseInt(this.node.style.left, 10) + parseInt(this.node.style.marginLeft, 10) + this.popupBorderWidth);
+		y = inEvent.pageY - (parseInt(this.node.style.top, 10) + parseInt(this.node.style.marginTop, 10) + this.popupBorderWidth);
+		return {x: x, y: y};
 	},
 	dragstartHandler: function(inSender, inEvent) {
-		//this.log('type:', inEvent.type, 'dx:', inEvent.dx, 'dy:', inEvent.dy, 'pageX:', inEvent.pageX, 'pageY:', inEvent.pageY);
 		var pos = this.dragGetStartPosition(inSender, inEvent);
 		this.$.dragger.applyStyle('top', pos.top + 'px');
 		this.$.dragger.applyStyle('left', pos.left + 'px');
 	},
 	dragHandler: function(inSender, inEvent) {
-		//this.log('type:', inEvent.type, 'dx:', inEvent.dx, 'dy:', inEvent.dy, 'pageX:', inEvent.pageX, 'pageY:', inEvent.pageY);
+		var c = this.canvasGetColorFromPosition(this.dragGetCanvasPosition(inSender, inEvent));
+		this.$.dragger.applyStyle('background-color', 'rgb(' + c.r + ', ' + c.g + ', ' + c.b + ')');
 		this.$.dragger.node.style.webkitTransform = 'translate3d(' + inEvent.dx + 'px, ' + inEvent.dy + 'px, 0)';
 	},
 	dragfinishHandler: function(inSender, inEvent) {
-		//this.log('type:', inEvent.type, 'dx:', inEvent.dx, 'dy:', inEvent.dy, 'pageX:', inEvent.pageX, 'pageY:', inEvent.pageY);
+		var c = this.canvasGetColorFromPosition(this.dragGetCanvasPosition(inSender, inEvent));
+		this.doColorSelect(c);
 	},
 	
 });
