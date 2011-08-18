40e2d9d6f8ba4cb9876dfb35cf856f69998e5156
oil <brandonvb@gmail.com>
2011-08-17 22:40:59 -0700

    ok, manually fix it, haha

--- enyo/wi/misc.js
+++ enyo/wi/misc.js
@@ -12,4 +12,15 @@ enyo.kind({
 		{name: 'label'},
 	],*/
 	
+	create: function(inProps) {
+		this.inherited(arguments);
+		
+		// for some reason, pushing my own chrome errors somewhere, so ill just fix their chrome after the fact
+		this.$.label.removeClass('enyo-listselector-label enyo-label');
+		this.applyStyle('position', 'relative');
+		this.$.itemContainer.applyStyle('margin-left', '22px');
+		this.$.arrow.applyStyle('position', 'absolute');
+		this.$.arrow.applyStyle('left', '0');
+	}
+	
 });
