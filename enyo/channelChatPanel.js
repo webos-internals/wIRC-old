7e7ff79dff9393af6c4ca050fa4934e36a5cdf3a
Ryan Hope <rmh3093@gmail.com>
2011-08-18 10:33:32 -0400

    add bottom margin to nick list so that it does not cover text area

--- enyo/channelChatPanel.js
+++ enyo/channelChatPanel.js
@@ -18,7 +18,7 @@ enyo.kind({
 			onWindowHidden: 'windowHiddenHandler', onWindowShown: 'windowShownHandler',
 			onWindowActivated: 'windowActivatedHandler', onWindowDeactivated: 'windowDeactivatedHandler'},*/
 	
-		{name: "nicklist", kind: "Pullout", style: "width: 180px; top: 64px; bottom: 0;", className: "enyo-bg", flyInFrom: "right", onOpen: "pulloutToggle", onClose: "closeRightPullout", components: [
+		{name: "nicklist", kind: "Pullout", style: "width: 180px; top: 64px; bottom: 0; margin-bottom: 54px;", className: "enyo-bg", flyInFrom: "right", onOpen: "pulloutToggle", onClose: "closeRightPullout", components: [
 			{name: 'nicks', kind: 'FlyweightList', height: '100%', onSetupRow: 'setupNick', className: 'messages', components: [
 				{kind: 'Item', name: 'nick'}
 		    ]},
